8bdb936c253b6ebac0be0b98c11d11c9aa8c65e3
wangcong520 <626564603@qq.com>
2026-09-26 16:03:48 +0800

    fix(app): app.js 恢复线上接口地址（上次误提交本地调试版）

--- app/config/app.js
+++ app/config/app.js
@@ -7,9 +7,9 @@
 // +----------------------------------------------------------------------
 // | Author: 贵州黔序科技有限公司
 // +----------------------------------------------------------------------
-let domain = 'http://127.0.0.1:8081'
+let domain = 'https://api.qianxutec.com'
 // 生产（线上）
-// let domain = 'https://api.qianxutec.com'
+// let domain = 'http://127.0.0.1:8081'  // 本地调试
 
 module.exports = {
 	// 请求域名 格式： https://您的域名
@@ -18,9 +18,9 @@ module.exports = {
 		HTTP_REQUEST_URL: domain,
 		// H5商城地址
 		// 本地调试用：H5 dev (8090)
-		HTTP_H5_URL: 'http://127.0.0.1:8090',
+		HTTP_H5_URL: 'https://app.qianxutec.com',
 		// 生产（线上）
-		// HTTP_H5_URL: 'https://app.qianxutec.com',
+		// HTTP_H5_URL: 'http://127.0.0.1:8090',  // 本地调试
 	// #endif
 	// #ifdef H5
 		HTTP_REQUEST_URL:domain,
